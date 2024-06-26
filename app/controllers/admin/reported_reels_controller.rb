0b3500ba0e5839cc4ec63b99a650ba084f780334
Manish Dahiya <manishdahiya0944@gmail.com>
2024-06-26 18:05:41 +0530

    added reported reel in dashboard

---
+++ app/controllers/admin/reported_reels_controller.rb
@@ -0,0 +1,27 @@
+class Admin::ReportedReelsController < Admin::AdminController
+  before_action :authenticate_user!
+  layout "admin"
+  def index
+    @reported_reels = ReportedReel.all.paginate(page: params[:page],per_page: 20).order(created_at: :desc)
+  end
+  def show
+    @reel = Reel.find(params[:id])
+  end
+  def destroy
+    @reportedReel = ReportedReel.find(params[:id])
+    @reel = Reel.find(@reportedReel.reel_id)
+    @reel.comments.destroy_all
+    Like.where(reel_id: @reel.id).destroy
+    @reel.destroy
+    @reportedReel.destroy
+    redirect_to admin_reported_reels_path, notice: 'Reel was successfully deleted.'
+  end
+
+  def approveReel
+    @reportedReel = ReportedReel.find(params[:id])
+    @reel = Reel.find(@reportedReel.reel_id)
+    @reel.update(isReported: false,report_count: 0)
+    @reportedReel.destroy
+    redirect_to admin_reported_reels_path, notice: 'Reel was successfully deleted.'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
     resources :users
     resources :reels
     resources :musics
+    resources :reported_reels
     put 'reels/:id/approveReel', to: 'reels#approveReel', as: 'approve_reel'
+    put 'reported_reels/:id/approveReel', to: 'reported_reels#approveReel', as: 'approve_reported_reel'
   end
 
 end
