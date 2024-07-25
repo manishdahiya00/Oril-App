515b61e8bef02779b299cbe4eeed86d0f54e5899
Manish Dahiya <manishdahiya0944@gmail.com>
2024-07-25 12:12:12 +0530

    bug fixes

--- app/controllers/api/v1/redeem.rb
+++ app/controllers/api/v1/redeem.rb
@@ -4,7 +4,7 @@ module API
       include API::V1::Defaults
 
       resource :coinWallet do
-        before {api_params}
+        before { api_params }
 
         params do
           use :common_params
@@ -24,18 +24,15 @@ module API
                 videoUpload: user.video_upload_coins.to_s,
                 fromYourFans: user.received_from_fans.to_s,
                 dailyCheckInCoins: "5",
-                uploadVideoCoins:"10",
+                uploadVideoCoins: "10",
                 shoppingCoins: [
-                  {coin: "5000",
-                  rupees: "50"
-                  },
-                  {coin: "10000",
-                  rupees: "100"
-                  },
-                  {coin: "20000",
-                  rupees: "200"
-                  },
-                ]
+                  { coin: "5000",
+                    rupees: "50" },
+                  { coin: "10000",
+                    rupees: "100" },
+                  { coin: "20000",
+                    rupees: "200" },
+                ],
               }
               { status: 200, message: "Success", wallet: wallet || {} }
             else
@@ -44,14 +41,12 @@ module API
           rescue Exception => e
             Rails.logger.error "API Exception - #{Time.now} - coinWallet - #{params.inspect} - Error - #{e}"
             { status: 500, message: "Error", error: e }
-
           end
         end
       end
 
-
       resource :addCash do
-        before {api_params}
+        before { api_params }
 
         params do
           use :common_params
@@ -65,11 +60,11 @@ module API
           begin
             user = User.find(params[:userId])
             if user.present?
-              user.orders.create(amount: params[:amount], payment_id: params[:paymentId], order_id: params[:orderId],receipt: params[:receipt])
+              user.orders.create(amount: params[:amount], payment_id: params[:paymentId], order_id: params[:orderId], receipt: params[:receipt])
               wallet_balance = user.wallet_balance + (params[:amount].to_i * 100)
               user.update(wallet_balance: wallet_balance)
-              user.transactions.create(transaction_name: "Added Coins",transaction_amount: params[:amount],transaction_coins: params[:amount].to_i * 100)
-              user.notifications.create(content: "Purchased #{params[:amount]} Rupees Coins, #{params[:amount].to_i * 100} Coins Added",notification_type: "transaction")
+              user.transactions.create(transaction_name: "Added Coins", transaction_amount: params[:amount], transaction_coins: params[:amount].to_i * 100)
+              user.notifications.create(content: "Purchased #{params[:amount]} Rupees Coins, #{params[:amount].to_i * 100} Coins Added", notification_type: "transaction")
               { status: 200, message: "Success", data: "Payment Successfull" }
             else
               { status: 500, message: "User Not Found" }
@@ -82,7 +77,7 @@ module API
       end
 
       resource :redeemList do
-        before {api_params}
+        before { api_params }
 
         params do
           use :common_params
@@ -101,7 +96,7 @@ module API
                   payoutId: payout.id,
                   payoutName: payout.payout_name,
                   payoutImageUrl: payout.payout_img_url,
-                  payoutAmounts: payout.payout_amount.split(',')
+                  payoutAmounts: payout.payout_amount.split(","),
                 }
               end
               data[:payoutList] = payout_list
@@ -116,9 +111,8 @@ module API
         end
       end
 
-
       resource :redeemSubmit do
-        before {api_params}
+        before { api_params }
 
         params do
           use :common_params
@@ -132,16 +126,16 @@ module API
           begin
             user = User.find(params[:userId])
             if user.present?
-             if user.wallet_balance >= params[:coins].to_i
-              user.redeems.create(number_or_email: params[:numberOrEmail], upi_id: params[:upiId], coins: params[:coins], payout_id: params[:payoutId],status: "PENDING")
-              wallet_balance = user.wallet_balance - params[:coins].to_i
-              user.update(wallet_balance: wallet_balance)
-              user.transactions.create(transaction_name: "Withdrawl Request",transaction_amount: params[:coins].to_f / 100,transaction_coins: params[:coins])
-              Notification.create(creator_id: user.id,content: "Withdrawl request for #{params[:coins]} coins submitted successfully. Approved Soon",notification_type: "transaction")
-              { status: 200, message: "Success", data: "Request Submitted, Approved Soon." }
-             else
-              { status: 500, message: "Not Enough Balance" }
-             end
+              if user.wallet_balance >= params[:coins].to_i
+                user.redeems.create(number_or_email: params[:numberOrEmail], upi_id: params[:upiId], coins: params[:coins], payout_id: params[:payoutId], status: "PENDING")
+                wallet_balance = user.wallet_balance - params[:coins].to_i
+                user.update(wallet_balance: wallet_balance)
+                user.transactions.create(transaction_name: "Withdrawl Request", transaction_amount: params[:coins].to_f / 100, transaction_coins: params[:coins])
+                Notification.create(creator_id: user.id, content: "Withdrawl request for #{params[:coins]} coins submitted successfully. Approved Soon", notification_type: "transaction")
+                { status: 200, message: "Success", data: "Request Submitted, Approved Soon." }
+              else
+                { status: 500, message: "Not Enough Balance" }
+              end
             else
               { status: 500, message: "User Not Found" }
             end
@@ -152,9 +146,8 @@ module API
         end
       end
 
-
       resource :sendGift do
-        before {api_params}
+        before { api_params }
 
         params do
           use :common_params
@@ -171,10 +164,10 @@ module API
                 user_balance = user.wallet_balance - params[:coins].to_i
                 creator_balance = creator.wallet_balance + params[:coins].to_i
                 received_from_fans = creator.received_from_fans + params[:coins].to_i
-                user.update(wallet_balance: user_balance,total_spending: params[:coins].to_i)
-                creator.update(wallet_balance: creator_balance,received_from_fans: received_from_fans)
-                user.transactions.create(transaction_name: "Sent Gift",transaction_amount: params[:coins].to_f / 100,transaction_coins: params[:coins])
-                Notification.create(user_id: creator.id,content: "#{user.user_name} gifted you #{params[:coins]} coins",notification_type: "gift",creator_id: user.id)
+                user.update(wallet_balance: user_balance, total_spending: params[:coins].to_i)
+                creator.update(wallet_balance: creator_balance, received_from_fans: received_from_fans)
+                user.transactions.create(transaction_name: "Sent Gift", transaction_amount: params[:coins].to_f / 100, transaction_coins: params[:coins])
+                Notification.create(user_id: creator.id, content: "#{user.user_name} gifted you #{params[:coins]} coins", notification_type: "gift", creator_id: user.id)
                 { status: 200, message: "Success", data: "Gift sent successfully" }
               else
                 { status: 500, message: "Not enough balance" }
@@ -189,9 +182,8 @@ module API
         end
       end
 
-
       resource :transactionHistory do
-        before {api_params}
+        before { api_params }
 
         params do
           use :common_params
@@ -202,11 +194,11 @@ module API
             user = User.find(params[:userId])
             if user.present?
               transactions = []
-              user.transactions.all.each do |transaction|
+              user.transactions.all.order(created_at: :desc).each do |transaction|
                 transactions << {
                   title: transaction.transaction_name,
                   subtitle: transaction.created_at.strftime("%d/%m/%y %I:%M %p"),
-                  coins: transaction.transaction_coins
+                  coins: transaction.transaction_coins,
                 }
               end
               { status: 200, message: "Success", data: transactions || [] }
@@ -219,11 +211,6 @@ module API
           end
         end
       end
-
-
-
-
-
     end
   end
 end
